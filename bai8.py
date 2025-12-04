cf16f1ecb4ce6dfe7989253219859a369e5b642e
phungvanhieu <phunghieu202@gmail.com>
2025-12-04 14:50:51 +0700

    duong da up file

---
+++ bai8.py
@@ -0,0 +1,37 @@
+import numpy
+
+arr = numpy.array([1, 2, 3])
+
+print(arr)
+import numpy as np
+
+# Sử dụng np thay cho numpy
+arr = np.array([1, 2, 3])
+
+print(arr)
+# định nghĩa bán kính r
+r = 0.43
+
+# import gói "math"
+import math
+
+# tính chu vi C
+C = 2 * math.pi * r
+
+# tính diện tích S
+S = math.pi * r **2
+
+# in kết quả
+print("Chu vi: " + str(C))
+print("Diện tích: " + str(S))
+# định nghĩa bán kính r
+r = 192500
+
+# nhập hàm radians trong gói math
+from  math import radians
+
+# dist = quãng đường Mặt Trăng di chuyển sau khi quay được 12 độ
+dist = radians(12) * r
+
+# in ra giá trị dist
+print(dist)
